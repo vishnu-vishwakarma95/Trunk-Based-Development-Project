07b79035edf197a5473ed24c275f64fc05948ba3
vishnu-vishwakarma95 <vishnu.vishwakarma@nashtechglobal.com>
2024-06-11 11:31:21 +0530

    just some meaningful change for testing file

--- feature-toggle.js
+++ feature-toggle.js
@@ -10,7 +10,7 @@ const features = {
   
   if (isFeatureEnabled('newFeature')) {
     // Code for the new feature
-    console.log(" New Feature Enabled  ");
+    console.log(" New Feature Enabled - added new line for testing");
   } else {
     // Code for the existing functionality
     console.log(" Existing Functionality ");
