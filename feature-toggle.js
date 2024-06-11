dd05e976cda5515e366563eb061aef8cd01abe0b
vishnu-vishwakarma95 <vishnu.vishwakarma@nashtechglobal.com>
2024-06-11 11:16:00 +0530

    just same meaningful change

--- feature-toggle.js
+++ feature-toggle.js
@@ -10,10 +10,10 @@ const features = {
   
   if (isFeatureEnabled('newFeature')) {
     // Code for the new feature
-    console.log("New Feature Enabled  ");
+    console.log(" New Feature Enabled  ");
   } else {
     // Code for the existing functionality
-    console.log("Existing Functionality ");
+    console.log(" Existing Functionality ");
   }
   
   module.exports = { isFeatureEnabled };
